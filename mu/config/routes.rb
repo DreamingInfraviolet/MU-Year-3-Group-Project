ea08f158d91ef6162520a543aa5767996990a76a
Anima <lightofanima@gmail.com>
2015-11-10 21:55:41 +0000

    Made further changes

--- mu/config/routes.rb
+++ mu/config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
 
   get 'pages/exercise_confirmation'
 
+  get 'home.html' => 'pages#homepage'
+
+  get 'login.html' => 'pages#sign_in'
+
   resources :tasks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
