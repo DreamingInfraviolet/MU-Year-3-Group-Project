c42dd5f423a9a1f75e3303af5572c4435dff4823
Anima <lightofanima@gmail.com>
2015-11-10 22:06:44 +0000

    Modified routes

--- mu/config/routes.rb
+++ mu/config/routes.rb
@@ -25,12 +25,14 @@ Rails.application.routes.draw do
 
   get 'login.html' => 'pages#sign_in'
 
+  post 'github/push'
+
   resources :tasks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#homepage'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
