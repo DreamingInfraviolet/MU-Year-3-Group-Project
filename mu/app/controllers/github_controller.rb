079a4ea88d2aa0d4011dcbf6f2ec51e8474fa80d
Anima <lightofanima@gmail.com>
2015-11-11 16:08:58 +0000

    Removed stupid empty readme

--- mu/app/controllers/github_controller.rb
+++ mu/app/controllers/github_controller.rb
@@ -1,5 +1,6 @@
 class GithubController < ApplicationController
   def push
     %x(echo `git pull origin master` >> ~/github_log.txt)
+    %x(echo `git push nuim master` >> ~/github_log_nuim.txt)
   end
 end
