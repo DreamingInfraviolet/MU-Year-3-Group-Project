7d4839cedaf8361b63ce15bb691c4972a3c68d53
Anima <lightofanima@gmail.com>
2015-11-22 15:02:27 +0000

    Added user model validation

--- mu/app/models/user.rb
+++ mu/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+    validates :first_name, presence: true
+    validates :second_name, presence: true
+    validates :user_name, presence:true, uniqueness: true
+    validates :age, presence: true
+    validates :email, presence: true
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password_hash, presence: true
 end
--- mu/test/models/user_test.rb
+++ mu/test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(first_name: "Anima", second_name: "Seteine", user_name: "LightOfAnima", password_hash: "asdfgh",
+                     age: "4 May 1995", email: "lightofanima@gmail.com");
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+ 
+
 end
